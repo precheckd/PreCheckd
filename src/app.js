79910a175a3d791653dc8c8ce4336bdbfb4eff99
Kent Mayer <kent@precheckd.com>
2026-09-20 15:33:19 -0400

    Moved max age on login to 30 days

--- src/app.js
+++ src/app.js
@@ -45,7 +45,7 @@ app.use(session({
   cookie: { 
     secure: process.env.NODE_ENV === 'production',
     httpOnly: true,
-    maxAge: 24 * 60 * 60 * 1000
+    maxAge: 30 * 24 * 60 * 60 * 1000
   }
 }));
 
